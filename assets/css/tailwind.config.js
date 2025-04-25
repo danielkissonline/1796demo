1b06cf038ca576bc4819db4a67ce92913ed673a1
Kiss Dániel <misc@danielkiss.online>
2025-04-25 21:22:10 +0200

    initial commit

---
+++ assets/css/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {},
+        screens: {
+            sm: '640px',
+            md: '1200px',
+            lg: '1200px',
+            xl: '1280px',
+            '2xl': '1536px',
+        },
+    },
+};
